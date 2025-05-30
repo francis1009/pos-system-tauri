8d742adc7eebbb78f72d19b83de548ce5c12eeb5
francis1009 <francism56789@gmail.com>
2025-05-31 00:38:15 +0800

    Integrate sqlite to frontend with plugin

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,21 @@
+use tauri_plugin_sql::{Migration, MigrationKind};
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_sql::Builder::new().build())
+        .plugin(
+            tauri_plugin_sql::Builder::default()
+                .add_migrations(
+                    "sqlite:pos_system.db",
+                    vec![Migration {
+                        version: 1,
+                        description: "Initial migration",
+                        kind: MigrationKind::Up,
+                        sql: include_str!("../migrations/v1.sql"),
+                    }],
+                )
+                .build(),
+        )
         .setup(|app| {
             if cfg!(debug_assertions) {
                 app.handle().plugin(
