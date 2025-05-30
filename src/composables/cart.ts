18e42ce32a853d6f5311ff41b77d45a571f96f58
Syoongy <mastersyong@gmail.com>
2025-05-30 23:43:39 +0800

    feat(cart.ts): add cart composable

---
+++ src/composables/cart.ts
@@ -0,0 +1,39 @@
+import type { CartItem, Item } from "@/types/item";
+import { readonly, ref } from "vue";
+
+const cart = ref<Map<string, CartItem>>(new Map());
+const total = ref(0);
+const isScanning = ref(true);
+
+function useCart() {
+	function addToCart(item: Item) {
+		const existingItem = cart.value.get(item.id);
+		if (existingItem) {
+			existingItem.quantity++;
+		} else {
+			const newItem: CartItem = { ...item, quantity: 1 };
+			cart.value.set(item.id, newItem);
+		}
+		calculateTotal();
+	}
+
+	function removeFromCart(itemId: string) {
+		cart.value.delete(itemId);
+		calculateTotal();
+	}
+
+	function calculateTotal() {
+		const cartItems = Array.from(cart.value.values());
+		total.value = cartItems.reduce((sum, item) => sum + item.price, 0);
+	}
+
+	return {
+		cart: readonly(cart),
+		total: readonly(total),
+		isScanning,
+		addToCart,
+		removeFromCart,
+	};
+}
+
+export { useCart };
