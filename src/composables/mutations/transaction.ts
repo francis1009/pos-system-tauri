9b80c84bffda9bb94f7291a7e4cdccef51bfefa9
Syoongy <mastersyong@gmail.com>
2025-06-03 04:02:02 +0800

    fix(mutations/transaction.ts): creation only returns transaction id

--- src/composables/mutations/transaction.ts
+++ src/composables/mutations/transaction.ts
@@ -1,19 +1,11 @@
-import { useMutation, useQueryClient } from "@tanstack/vue-query";
+import { useMutation } from "@tanstack/vue-query";
 import { useTransactionService } from "@/composables/service/transaction";
-import type { Transaction } from "@/types/transaction";
 
 const { addTransaction } = useTransactionService();
 
 const useTransactionMutation = () => {
-	const queryClient = useQueryClient();
-
 	const { mutateAsync: create } = useMutation({
 		mutationFn: addTransaction,
-		onSuccess: (data) => {
-			queryClient.setQueryData(["transactions"], (oldData: Array<Transaction> | undefined) =>
-				oldData ? [...oldData, data] : [data],
-			);
-		},
 	});
 
 	return {
