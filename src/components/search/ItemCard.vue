3f3a1c851688839d2a0c0ae8214cebffb76b9f04
Syoongy <mastersyong@gmail.com>
2025-06-10 16:01:06 +0800

    fix(itemcard.vue): import Item type and format defineEmits

--- src/components/search/ItemCard.vue
+++ src/components/search/ItemCard.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
 import { Card, CardContent } from "@/components/ui/card";
+import type { Item } from "@/types/item";
 
 const { item, formatter } = defineProps<{
 	item: Item;
 	formatter: Intl.NumberFormat;
 }>();
 defineEmits<{
-	(e: "selectItem", item: Item): void;
+	selectItem: [item: Item];
 }>();
 </script>
 
@@ -17,7 +18,7 @@ defineEmits<{
 	>
 		<CardContent class="px-4 h-full flex flex-col">
 			<div class="flex-1 min-w-0">
-				<p class="text-lg font-bold text-base truncate">{{ item.name }}</p>
+				<p class="text-lg font-bold truncate">{{ item.name }}</p>
 				<p class="text-sm text-gray-600">{{ formatter.format(item.price / 100) }}</p>
 				<p class="text-xs text-gray-400 truncate">{{ item.barcode }}</p>
 			</div>
